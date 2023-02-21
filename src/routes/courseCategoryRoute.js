42408fae3c113e1d916cba5593be50669cf4a5c9
Dion <riadidion@gmail.com>
2023-02-21 09:44:38 +0700

    refaktor route kategori kelas

--- src/routes/courseCategoryRoute.js
+++ src/routes/courseCategoryRoute.js
@@ -6,8 +6,16 @@ const router = express.Router();
 
 router.get("/", verify.authenticateToken, getAllCourseCategory);
 router.get("/:id", verify.authenticateToken, getCourseCategoryById);
-router.put("/:id", [verify.authenticateToken, body("name").isLength({ min: 3 }).withMessage('Nama kategori kurang dari 3 karakter')], updateCourseCategory);
-router.post("/", [verify.authenticateToken, body("name").isLength({ min: 3 }).withMessage('Nama kategori kurang dari 3 karakter')], createCourseCategory);
+router.put("/:id", [verify.authenticateToken, 
+    body("name")
+        .isLength({ min: 3 })
+        .withMessage('Nama kategori kurang dari 3 karakter')
+    ], updateCourseCategory);
+router.post("/", [verify.authenticateToken, 
+    body("name")
+    .isLength({ min: 3 })
+    .withMessage('Nama kategori kurang dari 3 karakter')
+    ], createCourseCategory);
 router.delete("/:id", verify.authenticateToken, deleteCourseCategory);
 
 module.exports = router;
