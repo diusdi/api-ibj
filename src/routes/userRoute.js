ca3201cf13b238ac685061c9e50f876dfd80e036
Dion <riadidion@gmail.com>
2023-02-22 08:39:46 +0700

    menambahkan validasi input update kelas

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,18 +6,26 @@ const router = express.Router();
 
 router.get("/", verify.authenticateToken, getAllUser);
 router.get("/:id", verify.authenticateToken, getUserById);
-router.post("/", [verify.authenticateToken, 
-    body("name")
-        .isLength({ min: 3 })
-        .withMessage('Nama peserta kurang dari 3 karakter'),
-    body("email")
-        .isEmail()
-        .withMessage('email peserta tidak valid'),
-    body("password")
-        .isLength({ min: 8 })
-        .withMessage('Password kategori kurang dari 8 karakter'),
-    ], createUser);
-router.put("/:id", verify.authenticateToken, updateUser);
+router.post(
+  "/",
+  [
+    verify.authenticateToken,
+    body("name").isLength({ min: 3 }).withMessage("Nama peserta kurang dari 3 karakter"),
+    body("email").isEmail().withMessage("email peserta tidak valid"),
+    body("password").isLength({ min: 8 }).withMessage("Password kategori kurang dari 8 karakter"),
+  ],
+  createUser
+);
+router.put(
+  "/:id",
+  [
+    verify.authenticateToken,
+    body("name").isLength({ min: 3 }).withMessage("Nama peserta kurang dari 3 karakter"),
+    body("email").isEmail().withMessage("email peserta tidak valid"),
+    body("password").isLength({ min: 8 }).withMessage("Password kategori kurang dari 8 karakter"),
+  ],
+  updateUser
+);
 router.delete("/:id", verify.authenticateToken, deleteUser);
 
 module.exports = router;
