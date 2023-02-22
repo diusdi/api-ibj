c597abc975db5bd617f7e6ad5741b6ad113f96f7
Dion <riadidion@gmail.com>
2023-02-22 11:05:39 +0700

    fix CRUD user course (Akses kelas peserta)

--- src/controllers/userCoursesController.js
+++ src/controllers/userCoursesController.js
@@ -7,31 +7,21 @@ exports.getAllUserCourse = (req, res, next) => {
 };
 
 exports.getUserCourseById = (req, res, next) => {
-    const querySearch = "SELECT user_courses.id, users.name, courses.title FROM users INNER JOIN user_courses ON users.id = user_courses.user_id INNER JOIN courses ON user_courses.course_id = courses.id WHERE user_courses.id = ?; ";
-
-  getDataById(res, querySearch, req.params.id);
+  getDataById(res, req.params.id);
 };
 
 exports.updateUserCourse = (req, res, next) => {
   const data = { ...req.body };
-  const querySearch = "SELECT * FROM user_courses WHERE id = ?";
   const queryUpdate = "UPDATE user_courses SET ? WHERE id = ?";
 
-  updateData(res, querySearch, queryUpdate, req.params.id, data);
+  updateData(res, req.params.id, data);
 };
 
 exports.createUserCourse = (req, res, next) => {
   const data = { ...req.body };
-//   const querySearch = "SELECT * FROM course_categories WHERE id = ?";
-  const querySql = "INSERT INTO user_courses SET ?";
-
-  // console.log(data.category);
-  insertData(res, querySql, data);
+  insertData(res, data);
 };
 
 exports.deleteUserCourse = (req, res) => {
-  const querySearch = 'SELECT * FROM user_courses WHERE id = ?';
-  const queryDelete = 'DELETE FROM user_courses WHERE id = ?';
-
-  deleteData(res, querySearch, queryDelete, req.params.id);
+  deleteData(res, req.params.id);
 };
--- src/models/userCourse.js
+++ src/models/userCourse.js
@@ -10,65 +10,102 @@ exports.getData = (response, statement) => {
   });
 };
 
-exports.getDataById = (response, searchStatement, id) => {
-  koneksi.query(searchStatement, [id], (err, rows, field) => {
+exports.getDataById = (response, id) => {
+  findUserCourse(response, id, "SEARCH");
+};
+
+exports.updateData = (response, id, data) => {
+  findUserCourse(response, id, "UPDATE");
+  findUser(response, data.user_id);
+  findCourse(response, data.course_id);
+  updateUserCourse(response, id, data);
+};
+
+exports.insertData = (response, data) => {
+  findUser(response, data.user_id);
+  findCourse(response, data.course_id);
+  createUserCourse(response, data);
+};
+
+exports.deleteData = (response, id) => {
+  findUserCourse(response, id, "DELETE");
+  deleteUserCourse(response, id);
+};
+
+const findUser = (response, id) => {
+  const queryUser = "SELECT * FROM users WHERE id = ?";
+  findQuery(response, queryUser, id, "Peserta");
+};
+
+const findCourse = (response, id) => {
+  const courseQuery = "SELECT * FROM courses WHERE id = ?";
+  findQuery(response, courseQuery, id, "Kelas");
+};
+
+const findQuery = (response, findQuery, id, findName) => {
+  koneksi.query(findQuery, id, (err, rows, field) => {
     if (err) {
-      return response.status(500).json({ message: "Ada kesalahan", error: err });
+      return responseMessage(response, 500, ["Ada kesalahan", { error: err }]);
+    }
+    if (rows.length) {
+      return;
+    } else {
+      return responseMessage(response, 404, `${findName} tidak ada`);
+    }
+  });
+};
+
+const findUserCourse = (response, id, action) => {
+  const userCourseQuery = "SELECT user_courses.id, users.name, courses.title FROM users INNER JOIN user_courses ON users.id = user_courses.user_id INNER JOIN courses ON user_courses.course_id = courses.id WHERE user_courses.id = ?; ";
+
+  koneksi.query(userCourseQuery, id, (err, rows, field) => {
+    if (err) {
+      return responseMessage(response, 500, ["Ada kesalahan", { error: err }]);
     }
 
     if (rows.length) {
-      responseData(response, 200, rows);
+      switch (action) {
+        case "SEARCH":
+          return responseData(response, 200, rows);
+        case "UPDATE":
+          return;
+        case "DELETE":
+          return;
+      }
     } else {
-      return response.status(404).json({ message: "Data tidak ditemukan!", success: false });
+      return responseMessage(response, 404, "Akses kelas peserta tidak ditemukan");
     }
   });
 };
 
-exports.updateData = (response, searchStatement, updateStatement, id, data) => {
-  koneksi.query(searchStatement, id, (err, rows, field) => {
-      if (err) {
-          return response.status(500).json({ message: 'Ada kesalahan', error: err });
-      }
-
-      if (rows.length) {
-          koneksi.query(updateStatement, [data, id], (err, rows, field) => {
-              if (err) {
-                  return response.status(500).json({ message: 'Ada kesalahan', error: err });
-              }
-
-              responseMessage(response, 200, 'Berhasil update data!');
-          });
-      } else {
-          return response.status(404).json({ message: 'Data tidak ditemukan!', success: false });
-      }
-  });
-};
-
-exports.insertData = (response, statement, data) => {
-  koneksi.query(statement, data, (err, rows, field) => {
+const createUserCourse = (response, data) => {
+  const insertQuery = "INSERT INTO user_courses SET ?";
+  koneksi.query(insertQuery, data, (err, rows, field) => {
     if (err) {
-      return response.status(500).json({ message: "Gagal menambahkan data", error: err });
+      return responseMessage(response, 500, "Gagal menambahkan data");
+    } else {
+      return responseMessage(response, 201, "Berhasil menambahkan data!");
     }
-    responseMessage(response, 201, "Berhasil menambahkan data!");
   });
 };
 
-exports.deleteData = (response, searchStatement, deleteStatement, id) => {
-  koneksi.query(searchStatement, id, (err, rows, field) => {
-      if (err) {
-          return response.status(500).json({ message: 'Ada kesalahan', error: err });
-      }
+const updateUserCourse = (response, id, data) => {
+  const updateStatement = "UPDATE user_courses SET ? WHERE id = ?";
+  koneksi.query(updateStatement, [data, id], (err, rows, field) => {
+    if (err) {
+      return responseMessage(response, 500, ["Ada kesalahan", { error: err }]);
+    }
 
-      if (rows.length) {
-          koneksi.query(deleteStatement, id, (err, rows, field) => {
-              if (err) {
-                  return response.status(500).json({ message: 'Ada kesalahan', error: err });
-              }
+    return responseMessage(response, 200, "Berhasil update data!");
+  });
+};
 
-              responseMessage(response, 200, 'Berhasil hapus data!');
-          });
-      } else {
-          return response.status(404).json({ success: false, message: 'Data tidak ditemukan!' });
-      }
+const deleteUserCourse = (response, id) => {
+  const deleteQuery = "DELETE FROM user_courses WHERE id = ?";
+  koneksi.query(deleteQuery, id, (err) => {
+    if (err) {
+      return responseMessage(response, 500, ["Gagal Menghapus data", { error: err }]);
+    }
+    responseMessage(response, 201, "Berhasil menghapus data!");
   });
 };
